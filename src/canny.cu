419dfab4195220f9889782e02ceebac26418354b
Arash Sal Moslehian <arashsm79@yahoo.com>
2022-07-04 11:59:46 +0430

    Initial CUDA commit

--- src/canny.cu
+++ src/canny.cu
@@ -1,8 +1,9 @@
 #include "canny.h"
-#include "omp.h"
 #include <cstring>
 #include <iostream>
 #include <math.h>
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
 
 using namespace std;
 
@@ -40,7 +41,6 @@ void gaussian_blur(const uint8_t *input_image, int height, int width,
   const double kernel[9] = {1, 2, 1, 2, 4, 2, 1, 2, 1};
   const int kernel_sum = 16;
 
-#pragma omp parallel for
   for (int col = OFFSET; col < width - OFFSET; col++) {
     for (int row = OFFSET; row < height - OFFSET; row++) {
       double output_intensity = 0;
@@ -65,7 +65,6 @@ void gradient_magnitude_direction(const uint8_t *input_image, int height,
   const int8_t Gx[] = {-1, 0, 1, -2, 0, 2, -1, 0, 1};
   const int8_t Gy[] = {1, 2, 1, 0, 0, 0, -1, -2, -1};
 
-#pragma omp parallel for
   for (int col = OFFSET; col < width - OFFSET; col++) {
     for (int row = OFFSET; row < height - OFFSET; row++) {
       double grad_x_sum = 0.0;
@@ -116,7 +115,6 @@ void non_max_suppression(double *gradient_magnitude,
                          uint8_t *gradient_direction, int height, int width,
                          double *output_image) {
   memcpy(output_image, gradient_magnitude, width * height * sizeof(double));
-#pragma omp parallel for
   for (int col = OFFSET; col < width - OFFSET; col++) {
     for (int row = OFFSET; row < height - OFFSET; row++) {
       int pixel_index = col + (row * width);
@@ -166,7 +164,6 @@ void non_max_suppression(double *gradient_magnitude,
 void thresholding(double *suppressed_image, int height, int width,
                   int high_threshold, int low_threshold,
                   uint8_t *output_image) {
-#pragma omp parallel for
   for (int col = 0; col < width; col++) {
     for (int row = 0; row < height; row++) {
       int pixel_index = col + (row * width);
--- src/main.cu
+++ src/main.cu
@@ -1,5 +1,4 @@
 #include "canny.h"
-#include "omp.h"
 #include "opencv4/opencv2/core/core.hpp"
 #include "opencv4/opencv2/highgui/highgui.hpp"
 #include "opencv4/opencv2/imgproc/imgproc.hpp"
@@ -10,15 +9,15 @@ using namespace std;
 void canny_edge_detect_wrapper(string, string, int, int);
 
 int main(int argc, char **argv) {
+  if(argc < 4)
+    cout << "Args: input_image output_image low_threshold high_threshold" << endl;
+
   string input_image_path = argv[1];
   string output_image_path = argv[2];
   int low_threshold = stoi(argv[3]);
   int high_threshold = stoi(argv[4]);
-  omp_set_dynamic(0);
-  // cout << "dynamic: " << omp_get_dynamic() << " n_threads: " << omp_get_max_threads() << endl;
   canny_edge_detect_wrapper(input_image_path, output_image_path, high_threshold,
                             low_threshold);
-  return 0;
 }
 
 void canny_edge_detect_wrapper(string input_image_path,
@@ -34,11 +33,8 @@ void canny_edge_detect_wrapper(string input_image_path,
   int width = gray_image.cols;
   int height = gray_image.rows;
   cv::Mat canny_image(height, width, CV_8UC1, cv::Scalar::all(0));
-  double start_time = omp_get_wtime();
   canny_edge_detect(gray_image.data, height, width, high_threshold,
                     low_threshold, canny_image.data);
-  double run_time = omp_get_wtime() - start_time;
-  cout << run_time * 1000 <<  endl;
 
   cv::imwrite(output_image_path, canny_image);
 }
